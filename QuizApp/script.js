9dd65178bebf4ee0ea1ab1ad1280e1e4599ca6bc
DMGamage <gamagemadhushan@gmail.com>
2023-05-04 17:15:07 +0530

    Finish timer Part

--- QuizApp/script.js
+++ QuizApp/script.js
@@ -111,3 +111,28 @@ let quiz = new Quiz(questions);
 
 // display questions
 displayQuestion();
+
+//Added Countdown
+
+let time = 10;
+let quizTimeInMinutes = time * 60 * 60;
+let quizTime = quizTimeInMinutes / 60;
+
+let counting = document.getElementById("count-down");
+
+function startCountdown() {
+    let quizTimer = setInterval(function() {
+        if (quizTime <= 0) {
+            clearInterval(quizTimer);
+            showScores();
+        } else {
+            quizTime--;
+            let sec = Math.floor(quizTime % 60);
+            let min = Math.floor(quizTime / 60) % 60;
+            counting.innerHTML = `TIME: ${min} : ${sec}`;
+        }
+    }, 1000);
+}
+
+startCountdown();
+
